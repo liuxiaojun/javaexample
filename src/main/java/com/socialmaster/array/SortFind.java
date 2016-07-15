e28e2185746cede74cd245501a029386e9ab50b6
刘晓军 <gb2312@163.com>
2016-07-15 17:43:59 +0800

    array sort and find

---
+++ src/main/java/com/socialmaster/array/SortFind.java
@@ -0,0 +1,25 @@
+package com.socialmaster.array;
+import java.util.Arrays;
+/**
+ * Created by liuxiaojun on 2016/7/15.
+ */
+public class SortFind {
+    public static void main(String[] args) {
+        int array[] = { 2, 5, -2, 6, -3, 8, 0, -7, -9, 4 };
+        Arrays.sort(array);
+        printArray("数组排序结果为", array);
+        int index = Arrays.binarySearch(array, 2);
+        System.out.println("元素 2  在第 " + index + " 个位置");
+    }
+
+    private static void printArray(String message, int array[]) {
+        System.out.println(message + ": [length: " + array.length + "]");
+        for (int i = 0; i < array.length; i++) {
+            if(i != 0){
+                System.out.print(", ");
+            }
+            System.out.print(array[i]);
+        }
+        System.out.println();
+    }
+}
