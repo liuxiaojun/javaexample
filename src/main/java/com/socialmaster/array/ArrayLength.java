a78caa8c5ce31b25962b964710e0096f688889ad
刘晓军 <gb2312@163.com>
2016-07-15 17:48:43 +0800

    array length

---
+++ src/main/java/com/socialmaster/array/ArrayLength.java
@@ -0,0 +1,12 @@
+package com.socialmaster.array;
+import java.util.Arrays;
+/**
+ * Created by liuxiaojun on 2016/7/15.
+ */
+public class ArrayLength {
+    public static void main(String[] args) {
+        String[][] data = new String[2][5];
+        System.out.println("第一维数组长度: " + data.length);
+        System.out.println("第二维数组长度: " + data[0].length);
+    }
+}
