df6e4936df9331de5c5b57fa0d2750354d4c92db
刘晓军 <gb2312@163.com>
2016-07-18 16:04:20 +0800

    get now time

---
+++ src/main/java/com/socialmaster/date/Now.java
@@ -0,0 +1,16 @@
+package com.socialmaster.date;
+
+/**
+ * Created by liuxiaojun on 2016/7/18.
+ */
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Now {
+    public static void main(String[] args) {
+        SimpleDateFormat sdf = new SimpleDateFormat();// 格式化时间
+        sdf.applyPattern("yyyy-MM-dd HH:mm:ss a");// a为am/pm的标记
+        Date date = new Date();// 获取当前时间
+        System.out.println("现在时间：" + sdf.format(date)); // 输出已经格式化的现在时间（24小时制）
+    }
+}
