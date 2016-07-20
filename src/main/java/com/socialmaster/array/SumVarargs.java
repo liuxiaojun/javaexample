36c312f362fb2ccc4700864f609c3a468a0d56d4
刘晓军 <gb2312@163.com>
2016-07-20 10:29:12 +0800

    不定长数组

---
+++ src/main/java/com/socialmaster/array/SumVarargs.java
@@ -0,0 +1,20 @@
+package com.socialmaster.array;
+
+/**
+ * Created by liuxiaojun on 2016/7/20.
+ */
+public class SumVarargs {
+    static int sumvarargs(int... intArrays){
+        int sum, i;
+        sum = 0;
+        for(i = 0; i < intArrays.length; i++) {
+            sum += intArrays[i];
+        }
+        return(sum);
+    }
+    public static void main(String[] args) {
+        int sum = 0;
+        sum = sumvarargs(new int[]{10,12,33});
+        System.out.println("数字相加之和为: " + sum);
+    }
+}
