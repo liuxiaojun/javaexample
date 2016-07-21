ab7e89653174c82cd11c7cb2416e0a60ce28da3e
刘晓军 <gb2312@163.com>
2016-07-21 11:03:49 +0800

    get property

---
+++ src/main/java/com/socialmaster/file/GetProperty.java
@@ -0,0 +1,11 @@
+package com.socialmaster.file;
+
+/**
+ * Created by liuxiaojun on 2016/7/21.
+ */
+public class GetProperty {
+    public static void main(String[] args) {
+        String curDir = System.getProperty("user.dir");
+        System.out.println(curDir);
+    }
+}
