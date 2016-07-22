950ca7423d597f652d0671ae956419df8f81e31b
刘晓军 <gb2312@163.com>
2016-07-22 14:34:24 +0800

    查看线程是否存活

---
+++ src/main/java/com/socialmaster/thread/TwoThreadAlive.java
@@ -0,0 +1,30 @@
+package com.socialmaster.thread;
+
+/**
+ * Created by liuxiaojun on 2016/7/22.
+ */
+public class TwoThreadAlive extends Thread {
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            printMsg();
+        }
+    }
+
+    public void printMsg() {
+        Thread t = Thread.currentThread();
+        String name = t.getName();
+        System.out.println("name=" + name);
+    }
+
+    public static void main(String[] args) {
+        TwoThreadAlive tt = new TwoThreadAlive();
+        tt.setName("Thread");
+        System.out.println("before start(), tt.isAlive()=" + tt.isAlive());
+        tt.start();
+        System.out.println("just after start(), tt.isAlive()=" + tt.isAlive());
+        for (int i = 0; i < 10; i++) {
+            tt.printMsg();
+        }
+        System.out.println("The end of main(), tt.isAlive()=" + tt.isAlive());
+    }
+}
